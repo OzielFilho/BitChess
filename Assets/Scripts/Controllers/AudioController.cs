dedd9d481ccd9783aec694992efa9155015493b3
OzielFilho <OzielFilho03@hotmail.com>
2023-09-26 22:28:49 -0300

    feat : audio board insert in audio controller

--- Assets/Scripts/Controllers/AudioController.cs
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -8,11 +8,21 @@ public class AudioController : MonoBehaviour
     public AudioClip[] backgroundMusic;
     void Start()
     {
-        int indexMusicRamdom = Random.Range(0, backgroundMusic.Length);
-        AudioClip backgroundMusicSelected = backgroundMusic[indexMusicRamdom];
-        audioSource.clip = backgroundMusicSelected;
 
-        audioSource.Play();
+    }
+
+    public void Play(MonoBehaviour monoBehaviour)
+    {
+        if (monoBehaviour == GetComponent<Board>())
+        {
+            int indexMusicRamdom = Random.Range(0, backgroundMusic.Length);
+            AudioClip backgroundMusicSelected = backgroundMusic[indexMusicRamdom];
+            audioSource.clip = backgroundMusicSelected;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+
     }
 
     void Update()
