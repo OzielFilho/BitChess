d580781935657c157ad11d2754e8a2b467550ab1
Gabriel Cleiton <gabriel.n@lyncas.net>
2023-10-16 20:50:47 -0300

    alteracao na MenuController

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Enums;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Scene dependencies")] 
+    [SerializeField] private NetworkManager networkManager;
+    
     [SerializeField] private string nameScene;
+    
     public void StartGame()
     {
         SceneManager.LoadScene(nameScene);
+    }
+
+    public void OnConnect()
+    {
+        networkManager.SetPlayerLevel(ChessLevel.Normal);
+        networkManager.Connect();
     }
 }
