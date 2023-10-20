9cbc7c283a3925cb3e18cda1a3683036f7a7b414
OzielFilho <OzielFilho03@hotmail.com>
2023-10-20 19:22:24 -0300

    feat : controller and selection of board sprite

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -6,13 +6,33 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private string nameScene;
-    public void StartGame()
+    [SerializeField] private GameObject options;
+    [SerializeField] private GameObject menu;
+    private void StartGame()
     {
         SceneManager.LoadScene(nameScene);
+    }
+
+    public void Select(int index)
+    {
+        PlayerPrefs.SetInt("SelectedPrefabBoard", index);
+        PlayerPrefs.Save();
+        StartGame();
     }
     public void QuitGame()
     {
         Debug.Log("Quit game ");
         Application.Quit();
+    }
+    public void OpenMenu()
+    {
+        menu.SetActive(false);
+        options.SetActive(true);
+    }
+    public void QuitMenu()
+    {
+        Debug.Log("Quit menu ");
+        menu.SetActive(true);
+        options.SetActive(false);
     }
 }
